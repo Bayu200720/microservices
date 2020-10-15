a2d501a3ba2fd3c71ed905f5009466a59ea9740e
bayu <bayuyudo20@gmail.com>
2020-10-15 23:09:03 +0700

    ok

--- menu-service/handler/menu.go
+++ menu-service/handler/menu.go
@@ -4,10 +4,9 @@ import (
 	"net/http"
 
 	"github.com/Bayu200720/microservices/utils"
-	//"github.com/wskurniawan/digitalent-microservice/utils"
 )
 
-func AddMenu(http.ResponseWriter, *http.Request) {
+func AddMenu(w http.ResponseWriter, r *http.Request) {
 	utils.WrapAPISuccess(w, r, "success", http.StatusOK)
 
 }
