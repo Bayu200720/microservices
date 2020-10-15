2ff0e99a4db36ab93c6491ab6b0e33e73718dac6
bayu <bayuyudo20@gmail.com>
2020-10-15 22:04:27 +0700

    menambahkan util

--- menu-service/handler/menu.go
+++ menu-service/handler/menu.go
@@ -1,7 +1,12 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Bayu200720/microservices/utils"
+)
 
 func AddMenu(http.ResponseWriter, *http.Request) {
+	utils.WrapAPISuccess(w, r, "success", http.StatusOK)
 
 }
